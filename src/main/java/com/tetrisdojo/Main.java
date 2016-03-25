4f0df5d7c7f8dba2f9ba4657b2c802788d409509
aslivets <slivets.aleksay@gmail.com>
2016-03-25 17:00:00 +0100

    init

---
+++ src/main/java/com/tetrisdojo/Main.java
@@ -0,0 +1,8 @@
+package com.tetrisdojo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("THIS IS SPAAAARTAAA!!!");
+    }
+}
---
+++ src/test/java/com/tetrisdojo/MainTest.java
@@ -0,0 +1,14 @@
+package com.tetrisdojo;
+
+import org.junit.Test;
+
+/**
+ * Created by jedyCoder on 25.03.2016.
+ * 2016
+ */
+public class MainTest {
+    @Test
+    public void testMain() throws Exception {
+        Main.main(null);
+    }
+}
